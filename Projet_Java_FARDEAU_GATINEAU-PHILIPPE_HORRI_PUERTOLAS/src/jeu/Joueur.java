10e250108cff030f57292ed939c4de2432c7e673
Axel Gatineau Philippe <axel.gatineau.philippe@gmail.com>
2019-01-21 14:08:59 +0100

    Ajout de constructeurs + fonctions dans Personnage

--- Projet_Java_FARDEAU_GATINEAU-PHILIPPE_HORRI_PUERTOLAS/src/jeu/Joueur.java
+++ Projet_Java_FARDEAU_GATINEAU-PHILIPPE_HORRI_PUERTOLAS/src/jeu/Joueur.java
@@ -2,9 +2,30 @@ package jeu;
 
 import java.util.ArrayList;
 
-public class Joueur extends Personne{
-	private ArrayList sac;
+public class Joueur extends Personne {
+	private ArrayList<Objet> sac;
 	private int capaciteSac;
 	
-	public void seDeplacer(Deplacement d) {}
+	public Joueur(int c) {
+		capaciteSac = c;
+		sac = new ArrayList<Objet>();
+	}
+
+	public void seDeplacer(Deplacement d) {
+	}
+
+	public String afficherContenu() {
+		String s = "";
+		for (Objet o : sac) {
+			s = s + o.toString();
+		}
+		return s;
+	}
+
+	public void ajouter(Objet o)
+	{
+		if (o != null) 
+			sac.add(o);
+		return; 
+	}
 }
--- Projet_Java_FARDEAU_GATINEAU-PHILIPPE_HORRI_PUERTOLAS/src/jeu/Objet.java
+++ Projet_Java_FARDEAU_GATINEAU-PHILIPPE_HORRI_PUERTOLAS/src/jeu/Objet.java
@@ -2,4 +2,9 @@ package jeu;
 
 public abstract class Objet {
 	private String nom;
+	
+	@Override 
+	public String toString() {
+		return nom;
+	}
 }
