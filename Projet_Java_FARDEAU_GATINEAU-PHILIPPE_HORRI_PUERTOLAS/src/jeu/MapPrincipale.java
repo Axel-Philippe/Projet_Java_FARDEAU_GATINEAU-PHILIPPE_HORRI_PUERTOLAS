c2fdb4d8f213438b4c24e1a1c202eb3b83c9cb2f
Axel Gatineau Philippe <axel.gatineau.philippe@gmail.com>
2019-01-21 15:47:55 +0100

    PersonneMapActivite

--- Projet_Java_FARDEAU_GATINEAU-PHILIPPE_HORRI_PUERTOLAS/src/jeu/MapPrincipale.java
+++ Projet_Java_FARDEAU_GATINEAU-PHILIPPE_HORRI_PUERTOLAS/src/jeu/MapPrincipale.java
@@ -1,5 +1,12 @@
 package jeu;
 
+import javafx.scene.image.Image;
+
 public class MapPrincipale extends Map{
+
+	public MapPrincipale(String n, Image i) {
+		super(n, i);
+		// TODO Auto-generated constructor stub
+	}
 
 }
