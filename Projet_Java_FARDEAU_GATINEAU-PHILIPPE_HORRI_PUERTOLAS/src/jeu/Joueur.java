28dde5ae07ed209228b380a562f16663d8bf1ab0
Lucas Horri <contact.lucashorri@gmail.com>
2019-02-20 15:45:36 +0100

    Joueur edit

--- Projet_Java_FARDEAU_GATINEAU-PHILIPPE_HORRI_PUERTOLAS/src/jeu/Joueur.java
+++ Projet_Java_FARDEAU_GATINEAU-PHILIPPE_HORRI_PUERTOLAS/src/jeu/Joueur.java
@@ -120,21 +120,19 @@ public class Joueur extends Personne {
 		case RIGHT:
 			this.deplacementDroite();
 			return null;
-		case E:
-			Carte me = this.getCarte();
-			ArrayList<Personnage> lp = this.getCarte().getLesPersonnages();
-			Personnage p;
-			for (int i = 0; i < lp.size(); i++) {
-				p = lp.get(i);
-				if (this.getPosition().getX() > p.getZone().getP1().getX()
-						&& this.getPosition().getX() < p.getZone().getP2().getX()
-						&& this.getPosition().getY() > p.getZone().getP1().getY()
-						&& this.getPosition().getY() < p.getZone().getP2().getY()) {
-					p.getEnigme().afficher();
-				}
-			}
-			return null;
 
+		case E: Carte me = this.getCarte(); ArrayList<Personnage> lp =
+			this.getCarte().getLesPersonnages(); Personnage p; for (int i = 0; i <
+			lp.size(); i++) { p = lp.get(i); if (this.getPosition().getX() >
+			p.getZone().getP1().getX() && this.getPosition().getX() <
+			p.getZone().getP2().getX() && this.getPosition().getY() >
+			p.getZone().getP1().getY() && this.getPosition().getY() <
+			p.getZone().getP2().getY()) { 
+				p.getEnigme().afficher(); 
+			} 
+			
+			} 
+			return null;
 		case R:
 			Carte m = this.getCarte();
 			ArrayList<Bonus> l = this.getCarte().getLesBonus();
