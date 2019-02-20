2c34dc3c8a71549418da6295d478135b4b5c5332
Axel Gatineau Philippe <axel.gatineau.philippe@gmail.com>
2019-02-20 16:59:34 +0100

    Enlevement de tt les import inutiles

--- Projet_Java_FARDEAU_GATINEAU-PHILIPPE_HORRI_PUERTOLAS/src/fenetre/MainFX.java
+++ Projet_Java_FARDEAU_GATINEAU-PHILIPPE_HORRI_PUERTOLAS/src/fenetre/MainFX.java
@@ -1,41 +1,8 @@
 package fenetre;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
-import jeu.Activite;
-import jeu.Bonus;
-import jeu.Carte;
-import jeu.Coffre;
-import jeu.Deplacement;
-import jeu.Enigme;
-import jeu.Ingredient;
 import jeu.Jeu;
-import jeu.Joueur;
-import jeu.MapAnnexe;
-import jeu.MapPrincipale;
-import jeu.Objet;
-import jeu.Personnage;
-import jeu.Position;
-import jeu.Zone;
 
 public class MainFX extends Application {
 
