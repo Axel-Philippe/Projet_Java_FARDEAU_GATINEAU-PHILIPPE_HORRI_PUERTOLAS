9ab6ad0d14251f43ddab6d84852f6df1771b411e
Axel Gatineau Philippe <axel.gatineau.philippe@gmail.com>
2019-03-20 09:59:42 +0100

    Ajout JoueurTest

---
+++ Projet_Java_FARDEAU_GATINEAU-PHILIPPE_HORRI_PUERTOLAS/src/test/JoueurTest.java
@@ -0,0 +1,120 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Axel1
+ *
+ */
+class JoueurTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link jeu.Joueur#deplacementHaut()}.
+	 */
+	@Test
+	void testDeplacementHaut() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link jeu.Joueur#deplacementBas()}.
+	 */
+	@Test
+	void testDeplacementBas() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link jeu.Joueur#deplacementGauche()}.
+	 */
+	@Test
+	void testDeplacementGauche() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link jeu.Joueur#deplacementDroite()}.
+	 */
+	@Test
+	void testDeplacementDroite() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link jeu.Joueur#deplacementHautLabyrinthe()}.
+	 */
+	@Test
+	void testDeplacementHautLabyrinthe() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link jeu.Joueur#deplacementBasLabyrinthe()}.
+	 */
+	@Test
+	void testDeplacementBasLabyrinthe() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link jeu.Joueur#deplacementGaucheLabyrinthe()}.
+	 */
+	@Test
+	void testDeplacementGaucheLabyrinthe() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link jeu.Joueur#deplacementDroiteLabyrinthe()}.
+	 */
+	@Test
+	void testDeplacementDroiteLabyrinthe() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link jeu.Joueur#seDeplacer(javafx.scene.input.KeyEvent)}.
+	 */
+	@Test
+	void testSeDeplacer() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link jeu.Joueur#ramasser(jeu.Objet)}.
+	 */
+	@Test
+	void testRamasser() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link jeu.Joueur#proposerSolution(jeu.Enigme, java.lang.String)}.
+	 */
+	@Test
+	void testProposerSolution() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link jeu.Joueur#Inventaire(javafx.stage.Stage)}.
+	 */
+	@Test
+	void testInventaire() {
+		fail("Not yet implemented");
+	}
+
+}
